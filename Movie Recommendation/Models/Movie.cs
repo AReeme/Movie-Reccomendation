10cc81d0504530e80894d9950c3c65a51cb67503
AReeme <areeme@student.neumont.edu>
2023-02-07 16:58:44 -0700

    Movie Model Created

---
+++ Movie Recommendation/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Recommendation.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int? Year { get; set; }
+
+        public float? Rating { get; set; }
+
+        public string? ReleaseDate { get; set; }
+
+        public string? Image { get; set; }
+
+        public string? Genre { get; set; }
+
+        public Movie()
+        { }
+
+        public Movie(string title, int? year, float? rating, string? release, string? image, string? genre)
+        {
+            Title = title;
+            Year = year;
+            Rating = rating;
+            ReleaseDate = release;
+            Image = image;
+            Genre = genre;
+        }
+    }
+}
