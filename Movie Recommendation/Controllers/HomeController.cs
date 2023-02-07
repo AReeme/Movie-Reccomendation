62876491b5d880188acbf6a81a2d8211e690c282
epiccreeper10 <87875284+epiccreeper10@users.noreply.github.com>
2023-02-07 16:41:17 -0700

    Real Submit
    Real Submit

--- Movie Recommendation/Controllers/HomeController.cs
+++ Movie Recommendation/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace Movie_Recommendation.Controllers
         {
             return View();
         }
+        public IActionResult MovieQuiz()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
