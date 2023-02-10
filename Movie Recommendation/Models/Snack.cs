77ef592f6df9b0cac53f6fa86df17b568e52a40e
AReeme <areeme@student.neumont.edu>
2023-02-09 18:11:51 -0700

    Snack Model Created

---
+++ Movie Recommendation/Models/Snack.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Recommendation.Models
+{
+    public class Snack
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Type { get; set; }
+
+        public bool? isGlutenFree { get; set; }
+
+        public Snack() {}
+
+        public Snack(int id, string? name, string? type, bool? isGlutenFree)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            this.isGlutenFree = isGlutenFree;
+        }
+    }
+}
