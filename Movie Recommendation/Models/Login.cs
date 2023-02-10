d380716e5751b2af3c0d3a4a78adcca31508cbcd
bfinne05 <100608199+bfinne05@users.noreply.github.com>
2023-02-09 18:24:48 -0700

    Login Model Created

---
+++ Movie Recommendation/Models/Login.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Primitives;
+using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Recommendation.Models
+{
+    public class Login
+    {
+
+        [Key]
+        int id { get; set; }
+
+        [Required]
+        string loginName { get; set; }
+        [Required]
+        string password { get; set; }
+
+        bool darkMode { get; set; }
+
+        public Login() { }
+
+        public Login(int id, string loginName, string password, bool darkMode)
+        {
+            this.id = id;
+            this.loginName = loginName;
+            this.password = password;
+            this.darkMode = darkMode;
+        }
+    }
+}
