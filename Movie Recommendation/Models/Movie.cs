d0ee50064ef1ba0f5b79ad2e9cb8a697273c200c
bfinne05 <100608199+bfinne05@users.noreply.github.com>
2023-02-16 17:59:38 -0700

    Updated Movie Model

--- Movie Recommendation/Models/Movie.cs
+++ Movie Recommendation/Models/Movie.cs
@@ -6,32 +6,46 @@ namespace Movie_Recommendation.Models
     {
         [Key]
         public int Id { get; set; }
-
+        public string ImdbId { get; set; }
         public string Title { get; set; }
+        public string OriginalTitle { get; set; }
+        public string Status { get; set; }
+        public string Tagline { get; set; }
+        public string Overview { get; set; }
+        public string Homepage { get; set; }
 
-        public int? Year { get; set; }
+        public string BackdropPath { get; set; }
+        public string PosterPath { get; set; }
 
-        public float? Rating { get; set; }
+        public bool Adult { get; set; }
 
-        public string? ReleaseDate { get; set; }
+        public List<string> BelongsToCollection { get; set; }
+        public List<string> Genres { get; set; }
 
-        public string? Image { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public long Revenue { get; set; }
+        public long Budget { get; set; }
+        public int Runtime { get; set; }
 
-        public string? Genre { get; set; }
+        public double Popularity { get; set; }
+        public double VoteAverage { get; set; }
+        public int VoteCount { get; set; }
+
+        public List<string> ProductionCompanies { get; set; }
+        public List<string> ProductionCountries { get; set; }
+        public List<string> SpokenLanguages { get; set; }
+
+        public string AlternativeTitles { get; set; }
+        public string Releases { get; set; }
+        public string Casts { get; set; }
+        public string Images { get; set; }
+        public string Keywords { get; set; }
+        public string Trailers { get; set; }
+        public string Translations { get; set; }
+        public string SimilarMovies { get; set; }
+        public string SearchContainer { get; set; }
+        public List<string> Changes { get; set; }
 
         public string? UserId { get; set; }
-
-        public Movie()
-        { }
-
-        public Movie(string title, int? year, float? rating, string? release, string? image, string? genre)
-        {
-            Title = title;
-            Year = year;
-            Rating = rating;
-            ReleaseDate = release;
-            Image = image;
-            Genre = genre;
-        }
     }
 }
