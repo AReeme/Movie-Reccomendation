352e15e60aebabf526e01e2e3c195e4bd4f7d412
bfinne05 <100608199+bfinne05@users.noreply.github.com>
2023-02-16 18:33:55 -0700

    Might have translated api

--- Movie Recommendation/Controllers/HomeController.cs
+++ Movie Recommendation/Controllers/HomeController.cs
@@ -4,11 +4,17 @@ using Movie_Recommendation.Data;
 using Movie_Recommendation.Models;
 using System.Diagnostics;
 using System.Security.Claims;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Movie_Recommendation.Controllers
 {
     public class HomeController : Controller
     {
+        private HttpClient client = new HttpClient();
+        private string key = "864f586643a869865bfc9ec19ca5fdba";
+        private string url = "https://api.themoviedb.org/3/movie/";
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -23,9 +29,18 @@ namespace Movie_Recommendation.Controllers
 
         [Authorize]
         [HttpGet]
-        public IActionResult RecommendMovie()
+        public async Task<IActionResult> RecommendMovie()
         {
-            return View();
+            string movieId = "315162";
+            var movie = await client.GetAsync(url + movieId + "?api_key=" + key);
+            if (movie.IsSuccessStatusCode)
+            {
+                Movie rMovie = JsonConvert.DeserializeObject<Movie>(await client.GetStringAsync(url + movieId + "?api_key=" + key));
+                string name = rMovie.Title;
+                return View("RecommendMovie", name);
+            }
+ 
+                return Content("this did not work stupid");
         }
 
         [Authorize]
