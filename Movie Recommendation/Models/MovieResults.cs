ef2d51a4e3229fd26c18053e46af09dab36c0958
bfinne05 <100608199+bfinne05@users.noreply.github.com>
2023-03-02 19:00:37 -0700

    Hopefully translated data

--- Movie Recommendation/Models/MovieResults.cs
+++ Movie Recommendation/Models/MovieResults.cs
@@ -8,46 +8,8 @@ namespace Movie_Recommendation.Models
         public int Id { get; set; }
         public int Page { get; set; }
         public Object[] Results { get; set; }
-        public string ImdbId { get; set; }
-        public string Title { get; set; }
-        public string OriginalTitle { get; set; }
-        public string Status { get; set; }
-        public string Tagline { get; set; }
-        public string Overview { get; set; }
-        public string Homepage { get; set; }
-
-        public string Backdrop_Path { get; set; }
-        public string Poster_Path { get; set; }
-
-        public bool Adult { get; set; }
-
-        public List<string> BelongsToCollection { get; set; }
-        public List<Object> Genres { get; set; }
-
-        public DateTime ReleaseDate { get; set; }
-        public long Revenue { get; set; }
-        public long Budget { get; set; }
-        public int Runtime { get; set; }
-
-        public double Popularity { get; set; }
-        public double VoteAverage { get; set; }
-        public int VoteCount { get; set; }
-
-        public List<string> ProductionCompanies { get; set; }
-        public List<string> ProductionCountries { get; set; }
-        public List<string> SpokenLanguages { get; set; }
-
-        public string AlternativeTitles { get; set; }
-        public string Releases { get; set; }
-        public string Casts { get; set; }
-        public string Images { get; set; }
-        public string Keywords { get; set; }
-        public string Trailers { get; set; }
-        public string Translations { get; set; }
-        public string SimilarMovies { get; set; }
-        public string SearchContainer { get; set; }
-        public List<string> Changes { get; set; }
-        public string? UserId { get; set; }
+        public int Total_Results { get; set; }
+        public int Total_Pages { get; set; }
 
     }
 }
